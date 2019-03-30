da086c8206dfc8f780d5853dc3e39e34616f2a9b
Kaweevis <Kaweevis@hotmail.com>
2019-03-30 21:13:34 +0700

    Add files via upload

--- homedetail/admin.py
+++ homedetail/admin.py
@@ -2,22 +2,5 @@ from django.contrib import admin
 
 # Register your models here.
 
-from homedetail.models import Room, DataType, Data
 
-
-@admin.register(Room)
-class RoomAdmin(admin.ModelAdmin):
-    list_display = ('RoomNumber', 'RoomDetail', 'UserID', 'status')
-
-
-@admin.register(DataType)
-class DataTypeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'DataType_Name', 'DataTypeDetail', 'Rate')
-    list_editable = ['Rate']
-
-
-@admin.register(Data)
-class DataAdmin(admin.ModelAdmin):
-    list_display = ('id', 'RoomId', 'Data_Type', 'DataDate', 'DataDeadLine', 'Detail', 'status', )
-    list_filter = ('Data_Type', 'DataDate', 'RoomId')
 
--- homedetail/views.py
+++ homedetail/views.py
@@ -1,57 +1,21 @@
-from homedetail.models import Room, DataType, Data
 from django.views import generic
 from django.views.generic import TemplateView
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 
 
-class RoomDetailView(generic.DetailView):
-    model = Room
-
-
-def view_room(request):
-    user = request.user
-    room = Room.objects.filter(UserID=user.id)
-    return render(request, 'homedetail/room_list.html', {'room_list': room})
-
-
-def data_list(request, pk, room_filter=None):
-    data = Data.objects.filter(RoomId=pk)
-    if room_filter:
-        room_filter = get_object_or_404(Room)
-        data = data.filter(RoomId=room_filter)
-    return render(request, 'homedetail/data_in_the_room.html', {'data': data})
-
-
-class DataDetailView(generic.DetailView):
-    model = Data
-
-
-class DataListView(generic.ListView):
-    model = Data
-    paginate_by = 10
-
-
-@login_required
 def index(request):
     """View function for home page of site."""
 
-    # Generate counts of some of the main objects
-    num_rooms = Room.objects.all().count()
-
-    # Available books (status = 'a')
-    room_avilable = Room.objects.filter(status__exact='a').count()
-
-    # The 'all()' is implied by default.
-    num_datas = Data.objects.count()
+    # number of employer
+    user_count = User.objects.all().count()
 
     context = {
-        'num_rooms': num_rooms,
-        'room_avilable': room_avilable,
-        'num_datas': num_datas,
+        'user_count': user_count,
     }
 
     # Render the HTML template index.html with the data in the context variable
